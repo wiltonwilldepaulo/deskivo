fa251b74ae015dc6ba1eb745fb2dab04536ecb64
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-03-09 20:00:47 -0400

    Commit

--- preload.js
+++ preload.js
@@ -1,6 +1,11 @@
-// preload.js
+// Indica que este é o arquivo preload.js, responsável por fazer a ponte segura 
+// entre a interface e o processo principal.
 const { contextBridge, ipcRenderer } = require("electron");
 
+// Usa o contextBridge para expor um objeto chamado electronAPI dentro do window da página.
 contextBridge.exposeInMainWorld("electronAPI", {
+    // Cria uma função chamada openPage que recebe o nome da página e envia esse
+    //  valor para o processo principal pelo canal "window:open-page".
     openPage: (pageName) => ipcRenderer.invoke("window:open-page", pageName)
 });
+
